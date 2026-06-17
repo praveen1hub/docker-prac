206ddf622600f4fa636e2c749f8ea5952495f9af
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2026-06-17 14:08:04 +0530

    docker run code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Dynamically set base path depending on environment
+const env = process.env.VITE_ENV || 'prod'
+
 export default defineConfig({
+  base:
+    env === 'dev'
+      ? '/prac/dev/'
+      : env === 'uat'
+      ? '/prac/uat/'
+      : '/prac/',
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  }
 })
